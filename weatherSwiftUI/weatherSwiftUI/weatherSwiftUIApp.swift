3482487b16f5068813f6dee28849a54287c20c61
long.dang1 <long.dang1@tda.company>
2024-05-08 16:05:35 +0700

    create project

---
+++ weatherSwiftUI/weatherSwiftUI/weatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherSwiftUIApp.swift
+//  weatherSwiftUI
+//
+//  Created by anh.nguyen3 on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weatherSwiftUI/weatherSwiftUIUITests/weatherSwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  weatherSwiftUIUITestsLaunchTests.swift
+//  weatherSwiftUIUITests
+//
+//  Created by anh.nguyen3 on 08/05/2024.
+//
+
+import XCTest
+
+final class weatherSwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
